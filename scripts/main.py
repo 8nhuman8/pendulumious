7f180c5610688a132f1a685b85312797ecb08d0a
Artyom Bezmenov <artem.bezmenov02@gmail.com>
2020-06-12 23:28:55 +0300

    Include Tkinter

--- scripts/main.py
+++ scripts/main.py
@@ -1,14 +1,44 @@
 import pygame
+import tkinter as tk
+
 from math import sin, cos, pi
+from random import randrange
+from os import environ
+from platform import system
+
 from utils import first_acceleration, second_acceleration
 
 
-pygame.init()
-pygame.display.set_caption('Pendulumious')
-
 WIDTH, HEIGTH = 500, 500
+
+
+root = tk.Tk()
+root.title('Pendulumious')
+root.resizable(False, False)
+
+embed = tk.Frame(root, width=WIDTH, height=HEIGTH)
+embed.pack(side=tk.LEFT)
+#embed.grid(row=0, column=0, rowspan=5, columnspan=3)
+
+#parameters = tk.Frame(root, width=500, height=500)
+#parameters.pack()
+
+tk.Label(root, text='gravity').pack(side=tk.LEFT, anchor='nw')
+gravity = tk.Entry(width=10)
+gravity.insert(tk.END, '1')
+gravity.pack(side=tk.LEFT, anchor='nw')
+
+
+environ['SDL_WINDOWID'] = str(embed.winfo_id())
+if system == 'Windows':
+    environ['SDL_VIDEODRIVER'] = 'windib'
+
+
+pygame.init()
+
 screen = pygame.display.set_mode((WIDTH, HEIGTH))
 clock = pygame.time.Clock()
+
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -17,13 +47,13 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
 GRAVITY = 1
-MASS1, MASS2 = 5, 6
-LENGTH1, LENGTH2 = 100, 110
+MASS1, MASS2 = 4, 5
+LENGTH1, LENGTH2 = 100, 100
 ORIGIN = (WIDTH // 2, HEIGTH // 3)
 
 add_offset = lambda x, y: (x + ORIGIN[0], ORIGIN[1] - y)
 
-angle1, angle2 = pi/2, pi/2
+angle1, angle2 = randrange(0, 10), randrange(0, 10)
 anglular_velocity1, angular_velocity2 = 0, 0
 scatter1, scatter2 = [], []
 running = True
@@ -59,10 +89,10 @@ while running:
     scatter2.append(add_offset(x2, y2))
 
 
-    screen.fill(WHITE)
+    screen.fill(GRAY)
 
     pygame.draw.aalines(
-        screen, GRAY, False, [
+        screen, WHITE, False, [
             ORIGIN,
             add_offset(x1, y1),
             add_offset(x2, y2)
@@ -80,7 +110,8 @@ while running:
 
 
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(30)
+    root.update()
 
 
     anglular_velocity1 += angular_acceleration1
