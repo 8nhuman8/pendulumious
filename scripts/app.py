5470113adf1880de273c1bf91b7d6fffcbf3e004
Artyom Bezmenov <artem.bezmenov02@gmail.com>
2020-06-13 15:33:52 +0300

    Refactoring
    Add 'WidgetsFrame' class derived from `tk.Frame`.
    Import from 'Tkinter' and 'pygame' only the necessary.

---
+++ scripts/app.py
@@ -0,0 +1,147 @@
+from pygame import init, QUIT
+from pygame import quit as pygame_quit
+from pygame.event import get
+from pygame.time import Clock
+from pygame.display import set_mode, flip
+from pygame.draw import aalines, circle
+from tkinter import Tk, Frame
+
+from math import sin, cos
+from random import randrange
+from os import environ
+from platform import system
+
+from utils import first_acceleration, second_acceleration
+from tk_widgets_frame import WidgetsFrame
+
+
+WIDTH, HEIGTH = 500, 500
+ORIGIN = (WIDTH // 2, HEIGTH // 3)
+add_offset = lambda x, y: (x + ORIGIN[0], ORIGIN[1] - y)
+
+
+root = Tk()
+root.title('Pendulumious')
+root.resizable(False, False)
+
+embed = Frame(root, width=WIDTH, height=HEIGTH)
+embed.grid(row=0, column=0)
+
+widgets = WidgetsFrame(root, width=WIDTH, height=HEIGTH)
+widgets.grid(row=0, column=1)
+widgets.grid_propagate(False)
+
+
+environ['SDL_WINDOWID'] = str(embed.winfo_id())
+if system == 'Windows':
+    environ['SDL_VIDEODRIVER'] = 'windib'
+
+
+init()
+
+screen = set_mode((WIDTH, HEIGTH))
+clock = Clock()
+
+
+gravity = None
+mass1, mass2 = None, None
+length1, length2 = None, None
+
+angle1, angle2 = randrange(0, 10), randrange(0, 10)
+anglular_velocity1, angular_velocity2 = 0, 0
+trail1, trail2 = [], []
+running = True
+
+
+while running:
+    for event in get():
+        if event.type == QUIT:
+            running = False
+
+
+    gravity = widgets.gravity.get()
+    fps = widgets.fps.get()
+    mass1 = widgets.mass1.get()
+    mass2 = widgets.mass2.get()
+    length1 = widgets.length1.get()
+    length2 = widgets.length2.get()
+    bg_color = (
+        widgets.bg_color_r.get(),
+        widgets.bg_color_g.get(),
+        widgets.bg_color_b.get()
+    )
+    dots_color = (
+        widgets.dots_color_r.get(),
+        widgets.dots_color_g.get(),
+        widgets.dots_color_b.get()
+    )
+    trail1_color = (
+        widgets.trail1_color_r.get(),
+        widgets.trail1_color_g.get(),
+        widgets.trail1_color_b.get()
+    )
+    trail2_color = (
+        widgets.trail2_color_r.get(),
+        widgets.trail2_color_g.get(),
+        widgets.trail2_color_b.get()
+    )
+
+    angular_acceleration1 = first_acceleration(
+        gravity,
+        mass1, mass2,
+        length1, length2,
+        angle1, angle2,
+        anglular_velocity1, angular_velocity2
+    )
+    angular_acceleration2 = second_acceleration(
+        gravity,
+        mass1, mass2,
+        length1, length2,
+        angle1, angle2,
+        anglular_velocity1, angular_velocity2
+    )
+
+    x1 = int(length1 * sin(angle1))
+    y1 = int(-length1 * cos(angle1))
+
+    x2 = int(x1 + length2 * sin(angle2))
+    y2 = int(y1 - length2 * cos(angle2))
+
+    trail1.append(add_offset(x1, y1))
+    trail2.append(add_offset(x2, y2))
+
+
+    screen.fill(bg_color)
+
+    aalines(
+        screen, (255,255,255), False, [
+            ORIGIN,
+            add_offset(x1, y1),
+            add_offset(x2, y2)
+        ]
+    )
+
+    if len(trail1) > 1:
+        aalines(screen, trail1_color, False, trail1)
+    if len(trail2) > 1:
+        aalines(screen, trail2_color, False, trail2)
+
+    circle(screen, dots_color, ORIGIN, 5)
+    circle(screen, dots_color, add_offset(x1, y1), mass1)
+    circle(screen, dots_color, add_offset(x2, y2), mass2)
+
+
+    flip()
+    clock.tick(fps)
+    root.update()
+
+
+    anglular_velocity1 += angular_acceleration1
+    angular_velocity2 += angular_acceleration2
+
+    angle1 += anglular_velocity1
+    angle2 += angular_velocity2
+
+
+pygame_quit
+quit()
---
+++ scripts/tk_widgets_frame.py
@@ -0,0 +1,79 @@
+from tkinter import Frame, IntVar, Label, Scale, HORIZONTAL
+
+class WidgetsFrame(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.width = kwargs.pop('width')
+        self.height = kwargs.pop('height')
+
+
+        self.gravity = IntVar(value=1)
+        self.fps = IntVar(value=30)
+
+        self.mass1 = IntVar(value=5)
+        self.mass2 = IntVar(value=5)
+
+        self.length1 = IntVar(value=100)
+        self.length2 = IntVar(value=100)
+
+        self.bg_color_r = IntVar(value=133)
+        self.bg_color_g = IntVar(value=133)
+        self.bg_color_b = IntVar(value=133)
+
+        self.dots_color_r = IntVar(value=0)
+        self.dots_color_g = IntVar(value=0)
+        self.dots_color_b = IntVar(value=0)
+
+        self.trail1_color_r = IntVar(value=255)
+        self.trail1_color_g = IntVar(value=0)
+        self.trail1_color_b = IntVar(value=0)
+
+        self.trail2_color_r = IntVar(value=0)
+        self.trail2_color_g = IntVar(value=255)
+        self.trail2_color_b = IntVar(value=0)
+
+
+        self.set_widgets()
+
+        for i in range(4):
+            self.grid_columnconfigure(i, weight=1)
+
+
+    def set_widgets(self):
+        Label(self, text='Gravity').grid(row=0, column=0)
+        Scale(self, from_=1, to=20, orient=HORIZONTAL, variable=self.gravity).grid(row=0, column=1)
+
+        Label(self, text='FPS').grid(row=0, column=2)
+        Scale(self, from_=1, to=144, orient=HORIZONTAL, variable=self.fps).grid(row=0, column=3)
+
+        Label(self, text='Mass 1').grid(row=1, column=0)
+        Scale(self, from_=1, to=20, orient=HORIZONTAL, variable=self.mass1).grid(row=1, column=1)
+
+        Label(self, text='Mass 2').grid(row=1, column=2)
+        Scale(self, from_=1, to=20, orient=HORIZONTAL, variable=self.mass2).grid(row=1, column=3)
+
+        Label(self, text='Length 1').grid(row=2, column=0)
+        Scale(self, from_=50, to=200, orient=HORIZONTAL, variable=self.length1).grid(row=2, column=1)
+
+        Label(self, text='Length 2').grid(row=2, column=2)
+        Scale(self, from_=50, to=200, orient=HORIZONTAL, variable=self.length2).grid(row=2, column=3)
+
+        Label(self, text='Background color').grid(row=3, column=0)
+        Scale(self, from_=0, to=255, orient=HORIZONTAL, variable=self.bg_color_r).grid(row=3, column=1)
+        Scale(self, from_=0, to=255, orient=HORIZONTAL, variable=self.bg_color_g).grid(row=3, column=2)
+        Scale(self, from_=0, to=255, orient=HORIZONTAL, variable=self.bg_color_b).grid(row=3, column=3)
+
+        Label(self, text='Dots color').grid(row=4, column=0)
+        Scale(self, from_=0, to=255, orient=HORIZONTAL, variable=self.dots_color_r).grid(row=4, column=1)
+        Scale(self, from_=0, to=255, orient=HORIZONTAL, variable=self.dots_color_g).grid(row=4, column=2)
+        Scale(self, from_=0, to=255, orient=HORIZONTAL, variable=self.dots_color_b).grid(row=4, column=3)
+
+        Label(self, text='Trail 1 color').grid(row=5, column=0)
+        Scale(self, from_=0, to=255, orient=HORIZONTAL, variable=self.trail1_color_r).grid(row=5, column=1)
+        Scale(self, from_=0, to=255, orient=HORIZONTAL, variable=self.trail1_color_g).grid(row=5, column=2)
+        Scale(self, from_=0, to=255, orient=HORIZONTAL, variable=self.trail1_color_b).grid(row=5, column=3)
+
+        Label(self, text='Trail 2 color').grid(row=6, column=0)
+        Scale(self, from_=0, to=255, orient=HORIZONTAL, variable=self.trail2_color_r).grid(row=6, column=1)
+        Scale(self, from_=0, to=255, orient=HORIZONTAL, variable=self.trail2_color_g).grid(row=6, column=2)
+        Scale(self, from_=0, to=255, orient=HORIZONTAL, variable=self.trail2_color_b).grid(row=6, column=3)
